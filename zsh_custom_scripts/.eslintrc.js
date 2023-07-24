2be557d2de6b7baedcb1b24f1a9aa4f36ede8dde
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2023-07-24 14:21:04 +0300

    added eslintrc file for eslint lsp

---
+++ zsh_custom_scripts/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'extends': [
+        'standart'
+    ]
+}
+
+
+// tip: eslint-disable-line
+// tip: eslint-disable-next-line
+// tip: eslint-disable-previous-line
+// tip: eslint-disable - works for a block of code
+// tip: eslint-enable - enable again after block of code
