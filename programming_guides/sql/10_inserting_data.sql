a06f1dceca02c2dfc904882997117bdf8d57643f
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2024-03-14 03:50:20 +0200

    added more sql tips

--- programming_guides/sql/10_inserting_data.sql
+++ programming_guides/sql/10_inserting_data.sql
@@ -10,3 +10,17 @@ INSERT INTO mytable
 VALUES (value_or_expr, another_value_or_expr, …),
       (value_or_expr_2, another_value_or_expr_2, …),
       …;
+
+-- psql extension, will fill column from left, the rest will be default, works only in postgres,
+-- the default one is listing all the known columns that dont have default values
+INSERT INTO products VALUES (1, 'Cheese');
+
+-- you can also explicitly set DEFAULT, it might be more pleasant
+INSERT INTO products (product_no, name, price) VALUES (1, 'Cheese', DEFAULT);
+-- psql extension, works only in postgres
+INSERT INTO products DEFAULT VALUES;
+
+-- it is also possible to insert the result of a query (which might be no rows, one row, or many rows):
+INSERT INTO products (product_no, name, price)
+SELECT product_no, name, price FROM new_products
+WHERE release_date = 'today';
