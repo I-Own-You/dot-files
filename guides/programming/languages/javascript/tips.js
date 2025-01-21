cc009f5480fb8b4afc0f9a3d0ceb4345439d52bd
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2025-01-21 03:37:59 +0200

    added new tip guide/js

--- guides/programming/languages/javascript/tips.js
+++ guides/programming/languages/javascript/tips.js
@@ -1,8 +1,3 @@
-// all primitive types are actually wrapped by its classes objects, Number(), String(), .etc,
-// so it means you can wrap primitives with its classed and have methods on it,
-// variables that containt primitives also have some methods, but limited amount.
-const exampleA = 2 // same as Number(2), but not const a1 = Number(2), in this case its an object of Number class
-
 // == - used to check values, === - used to check values and types also
 console.log(2 == '2') // true
 console.log(2 === '2') // false
@@ -20,18 +15,7 @@ function z() {
 x(), z() // 2
 // x() and z() will execute form left to right but only the last will return the result
 
-// primitive values are compared by its values
-const primitiveA = 2
-const primitiveB = '2'
-// true, but they differ right ? this is why you must always use ===(triple equal),
-// it checks for both value and type
-console.log(primitiveA == primitiveB)
-
 // window - a global object in browser, you can think of a browser itself as window, or global in nodejs
-// you can access variables defnied with var like this: var a = 1,
-// window.a, but you cant access let and const like this
-var myWindowVar = 1
-console.log(window.myWindowVar)
 
 // 'use strict' - turns on the strict mode so that javascript gets less error prone and less confuse.
 // it must be put on the first line of the file, cant be turned off after it.
@@ -42,7 +26,6 @@ console.log(window.myWindowVar)
 //    3. reassign a value to an attribute of an object if it has writable=false descriptor
 //    4. cant use the same name for paramteres in functions
 //    5. [this] is no longer attached to global/window object
-//    6. cant use some reserved keys like: import, yield, let, .etc
 // you can use 'use strict' after some code, but it wont turn on the strict mode.
 // above 'use strict' can be placed only comments.
 
@@ -115,10 +98,11 @@ delete someUser?.name // will delete the attribute if user object exist
 
 // get a type from a variable/value
 // typeof [variable/value] === '[type]' -- return true/false
-// [variable/value] instanceof [type] -- return true/false
 const checkTypeOfVar = 'a'
 if (typeof checkTypeOfVar == 'string') console.log(true)
 if (typeof 'a' == 'string') console.log(true)
+// get true/false if an object/class is child of another class
+// [variable/value] instanceof [type] -- return true/false
 class Ala {}
 const alaObj = new Ala()
 if (alaObj instanceof Ala) console.log(true)
