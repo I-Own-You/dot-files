17defd00f9b39a705c95336fe52987df2a18aa25
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2025-09-21 21:14:27 +0300

    removed a script and changed install script

--- scripts/os_fresh_install.sh
+++ scripts/os_fresh_install.sh
@@ -19,10 +19,11 @@ git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antid
 
 echo "[*] Setting up dotfiles..."
 
+ln -sf "$HOME/dot-files/dot-files/.zprofile" "$HOME/.zprofile"
 ln -sf "$HOME/dot-files/dot-files/.zshrc" "$HOME/.zshrc"
 ln -sf "$HOME/dot-files/dot-files/.zsh_plugins.txt" "$HOME/.zsh_plugins.txt"
 ln -sf "$HOME/dot-files/dot-files/.gitconfig" "$HOME/.gitconfig"
-ln -sf "$HOME/dot-files/dot-files/yazi" "$HOME/.config/yazi"
+ln -sf "$HOME/dot-files/cli-tools/yazi" "$HOME/.config/yazi"
 
 sudo ln -sf "$HOME/dot-files/keybindings-related/keyd/default.conf" /etc/keyd/default.conf
 
