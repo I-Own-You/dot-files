bf9753588d7c883a15a06aa4a74af298149f0a3c
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2025-10-12 13:12:45 +0300

    changed path for rofi

--- linux-post-install/setup/distro/arch/wm/i3/scripts/rofi_toggle.sh
+++ linux-post-install/setup/distro/arch/wm/i3/scripts/rofi_toggle.sh
@@ -1,5 +1,5 @@
 if pgrep -x rofi > /dev/null; then
     pkill -x rofi
 else
-    /home/mkc/dot-files/linux-post-install/setup/launchers/rofi/launchers/type-4/launcher.sh
+    /home/mkc/dot-files/linux-post-install/setup/distro/arch/wm/i3/launchers/rofi/launchers/type-4/launcher.sh
 fi
