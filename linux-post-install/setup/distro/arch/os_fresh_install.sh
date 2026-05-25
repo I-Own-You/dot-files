eb1be54111d7c4b7707e2b0599ec796cf252ce77
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2026-05-25 20:44:21 +0300

    modified os_fresh_install

--- linux-post-install/setup/distro/arch/os_fresh_install.sh
+++ linux-post-install/setup/distro/arch/os_fresh_install.sh
@@ -19,17 +19,35 @@ ln -sf "$HOME/dot-files/terminals/wezterm" "$HOME/.config/wezterm"
 sudo ln -sf "$HOME/dot-files/keybindings-related/keyd/default.conf" /etc/keyd/default.conf
 
 echo  "[*] Install pacman packages..."
-sudo pacman -S git zsh unrar unzip xclip keyd nvim zoxide yazi eza bat git-delta fzf ripgrep npm github-cli glab p7zip fd uv nwg-look tree-sitter tree-sitter-cli
-sudo pacman -S keepassxc vlc syncthing wezterm discord obs-studio qbittorrent krita
+sudo pacman -S git zsh unrar unzip zip xclip keyd nvim neovide zoxide yazi eza bat git-delta fzf ripgrep npm github-cli glab p7zip fd uv nwg-look tree-sitter tree-sitter-cli grim flameshot mission-center keepassxc vlc syncthing wezterm discord obs-studio qbittorrent krita qt5ct qt6ct qt5-wayland qt6-wayland kvantum kvantum-qt5 kvantum-qt6
+paru google-chrome xnviewmp spotify vicinae-bin
 
-echo  "[*] Install aur packages..."
-paru google-chrome xnviewmp spotify
+echo "[*] Install apps from off sites..."
+# telegram
+
+# gnome
+sudo pacman -S dconf-editor gnome-tweaks gdm-settings 
+# gsettings
+gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+# extension
+https://extensions.gnome.org/extension/19/user-themes/
+https://extensions.gnome.org/extension/5090/space-bar/
+https://extensions.gnome.org/extension/973/switcher/ # only if not using vicinae
+https://extensions.gnome.org/extension/3843/just-perfection/
+https://extensions.gnome.org/extension/615/appindicator-support/
+https://extensions.gnome.org/extension/3396/color-picker/
+https://extensions.gnome.org/extension/16/auto-move-windows/
+https://extensions.gnome.org/extension/307/dash-to-dock/
+https://extensions.gnome.org/extension/779/clipboard-indicator/ # only if not using vicinae
+https://extensions.gnome.org/extension/7406/spotify-controls/
+# dots
+ln -sf "$HOME/dot-files/linux-post-install/setup/distro/arch/wm/hyprland/flameshot" "$HOME/.config/flameshot"
 
 # i3
 sudo pacman -S btop udiskie udisks2 playerctl polkit-gnome-authentication-agent gnome-keyring xdg-desktop-portal xdg-desktop-portal-gtk xcolor
-# i3 styling
+# styling
 sudo pacman -S nwg-look qt5ct qt6ct kvantum kvantum-qt5 mint-themes cachyos/ant-dracula-kvantum-theme-git # styles
-# i3 dots
+# dots
 ln -sf "$HOME/dot-files/linux-post-install/setup/distro/arch/wm/i3" "$HOME/.config/i3"
 ln -sf "$HOME/dot-files/linux-post-install/setup/distro/arch/wm/i3/compositors/picom" "$HOME/.config/picom"
 ln -sf "$HOME/dot-files/linux-post-install/setup/distro/arch/wm/i3/launchers/rofi" "$HOME/.config/rofi"
@@ -38,10 +56,9 @@ ln -sf "$HOME/dot-files/linux-post-install/setup/distro/arch/wm/i3/styling/gtk-3
 
 # hyprland
 sudo pacman -S btop udiskie udisks2 playerctl polkit-kde-agent xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-hyprland swaync nmrs overskride grim flameshot polkit-gnome hyprpaper waypaper hyprpicker dolphin nautilus hyprlock hyprsunset mission-center
-paru vicinae-bin
-# hyprland styling
+# styling
 sudo pacman -S qt5ct qt6ct kvantum kvantum-qt5 nwg-look qt5-wayland qt6-wayland
-# hyprland dots
+# dots
 ln -sf "$HOME/dot-files/linux-post-install/setup/distro/arch/wm/hyprland/hypr" "$HOME/.config/hypr"
 ln -sf "$HOME/dot-files/linux-post-install/setup/distro/arch/wm/hyprland/swaync" "$HOME/.config/swaync"
 ln -sf "$HOME/dot-files/linux-post-install/setup/distro/arch/wm/hyprland/flameshot" "$HOME/.config/flameshot"
@@ -60,9 +77,6 @@ sudo systemctl enable syncthing@$(whoami).service
 sudo systemctl start syncthing@$(whoami).service
 # keyd
 sudo systemctl enable --now keyd
-
-echo "[*] Install apps from off sites..."
-# telegram
 
 echo "[*] Side packages in case you need them..."
 # easyeffects (plugins for pipewire apps, audio manager)
