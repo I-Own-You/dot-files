980509b0d099f5f2da03a2f712c8ce56c1ec9ed5
I-Own-You <idkwhatsnextbutwellfaceit@gmail.com>
2026-07-26 20:20:36 +0300

    changed hyprland.sh file

--- linux-post-install/setup/distro/arch/setup_scripts_per_de/hyprland.sh
+++ linux-post-install/setup/distro/arch/setup_scripts_per_de/hyprland.sh
@@ -1,5 +1,5 @@
 # hyprland
-sudo pacman -S btop udiskie udisks2 playerctl xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-hyprland swaync nmrs overskride-bin grim flameshot polkit-gnome hyprpaper waypaper hyprpicker dolphin hyprlock hyprsunset mission-center
+sudo pacman -S btop udiskie udisks2 playerctl xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-hyprland swaync nmrs overskride-bin grim flameshot polkit-gnome hyprpaper waypaper hyprpicker dolphin hyprlock hyprsunset
 # styling
 sudo pacman -S qt5ct qt6ct kvantum kvantum-qt5 nwg-look qt5-wayland qt6-wayland
 # dont forget to make dark in gtk settings + qt5/6 + kvantum 
